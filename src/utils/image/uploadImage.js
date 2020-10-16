7896ef7e2104ed9132fcc5920a8505caf83ad805
lefcott19 <leandro.cotti@widergy.com>
2020-10-16 09:24:46 -0300

    added catch to uploadimage

--- src/utils/image/uploadImage.js
+++ src/utils/image/uploadImage.js
@@ -2,6 +2,16 @@ const { default: axios } = require('axios');
 
 const fs = require('fs');
 
+const auths = ['Client-ID 2a49d60e8b3f4ac'];
+let authIndex = 0;
+
+const getAuth = () => {
+  const auth = auths[authIndex];
+  authIndex += 1;
+  if (authIndex >= auths.length) authIndex = 0;
+  return auth;
+};
+
 module.exports = async path =>
   new Promise(resolve => {
     const image = fs.readFileSync(path).toString('base64');
@@ -9,7 +19,7 @@ module.exports = async path =>
       .post(
         'https://api.imgur.com/3/image',
         { image },
-        { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+        { headers: { 'Content-Type': 'application/x-www-form-urlencoded', Authorization: getAuth() } }
       )
       .then(response => resolve(response.data.data.link))
       .catch(error => {
