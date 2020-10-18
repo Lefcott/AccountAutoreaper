41efc6ce16c4169e5391ea8dcc11602e2c9b3cd2
lefcott19 <leandro.cotti@widergy.com>
2020-10-18 17:44:56 -0300

    docs

--- src/utils/images/setWallpaperText.js
+++ src/utils/images/setWallpaperText.js
@@ -1,6 +1,7 @@
 const Jimp = require('jimp');
 const wallpaper = require('wallpaper');
 
+/** @param {string} text @param {number} x @param {number} y  */
 module.exports = (text, x = 5, y = 5) =>
   new Promise(async resolve => {
     const image = await Jimp.read(`${__dirname}/wallpaper_base.png`);
