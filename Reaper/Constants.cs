213039cd67906d0581be6539d409cb2c161587d2
Lefcott <leandro.cotti@widergy.com>
2020-11-11 09:09:55 -0300

    add dotnet reaper

---
+++ Reaper/Constants.cs
@@ -0,0 +1,8 @@
+namespace Reaper
+{
+  public static class Constants
+  {
+    public const string LolPath = @"C:\Riot Games\League of Legends\LeagueClient_English.lnk";
+    public static string[] LolProcessNames = { "LeagueClient", "LeagueClientUx", "LeagueClientUxRender", "RiotClientServices", "RiotClientUx", "RiotClientUxRender" };
+  }
+}
---
+++ Reaper/Utils.cs
@@ -0,0 +1,36 @@
+using System.Diagnostics;
+using System;
+using System.Threading.Tasks;
+
+namespace Reaper
+{
+  public class Utils
+  {
+    static Process lolProcess = new Process();
+    public static async Task ResetLOL()
+    {
+      CloseLOL();
+      await Task.Delay(1000);
+      OpenLOL();
+    }
+    public static void OpenLOL()
+    {
+      lolProcess.StartInfo.UseShellExecute = true;
+      lolProcess.StartInfo.FileName = Constants.LolPath;
+      lolProcess.Start();
+    }
+    public static void CloseLOL()
+    {
+      foreach (string processName in Constants.LolProcessNames)
+      {
+        Process[] subProcesses = Process.GetProcessesByName(processName);
+        foreach (Process subProcess in subProcesses)
+        {
+          Console.WriteLine(@"Closing {subProcess.ProcessName}");
+          subProcess.CloseMainWindow();
+          subProcess.Close();
+        }
+      }
+    }
+  }
+}
